c4d03ec60ae9933053da0ffe09c8a189284bc4e3
alanreji <alanreji.pulickal@gmail.com>
2018-02-10 18:58:05 +0530

    cloud firestore implementation tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { AngularFirestore,AngularFirestoreCollection,AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import {  } from 'rxjs/add/operator/map';
+
+interface Message {
+  admin_id: string;
+  body: string;
+  end_time: string;
+  start_time: string;
+  subject: string;
+  title: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +19,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  msgscol: AngularFirestoreCollection<Message>;
+  msgs: Observable<Message[]>;
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit(){
+    this.msgscol = this.afs.collection('message');
+    this.msgs = this.msgscol.valueChanges();
+  }
+
 }
