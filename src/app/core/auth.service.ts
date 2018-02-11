caa7b6ec841a01c211e4b5465f3de33a0c9bbd85
alanreji <alanreji.pulickal@gmail.com>
2018-02-11 20:12:35 +0530

    google authentication success

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -12,7 +12,7 @@ import 'rxjs/add/operator/switchMap';
 interface User {
   uid: string,
   email: string,
-  photoUrl?: string,
+  photoURL?: string,
   displayName?: string,
 }
 
@@ -27,7 +27,7 @@ export class AuthService {
     this.user = this.afAuth.authState
       .switchMap((user) => {
         if (user) {
-          return this.afs.doc<User>('users/${user.uid}').valueChanges();
+          return this.afs.doc<User>('users/'+user.uid).valueChanges();
         } else {
           return Observable.of(null);
         }
@@ -50,15 +50,14 @@ export class AuthService {
     //sets user data to firestore on login
 
     const userRef: AngularFirestoreDocument<User> = this.afs
-      .doc('users/${user.uid}');
+      .doc('users/'+user.uid);
 
     const data: User = {
       uid: user.uid,
       email: user.email,
       displayName: user.displayName,
-      photoUrl: user.photoUrl
+      photoURL: user.photoURL
     }
-
     return userRef.set(data);
   }
 
